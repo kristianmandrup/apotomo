4d02781da1f65b9889749ebc6c4e6ba4df60b790
Kurakin Alexander <kuraga333@mail.ru>
2013-03-22 09:26:16 +0400

    fix 7
    Restore JqueryHelper

--- lib/apotomo/javascript_generator.rb
+++ lib/apotomo/javascript_generator.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext" # for Hash.to_json etc.
 
 module Apotomo
   class JavascriptGenerator
+    autoload :JqueryHelper, 'apotomo/javascript_generator/jquery_helper'
+
     def initialize(framework)
       raise "No JS framework specified" if framework.blank?
       extend "apotomo/javascript_generator/#{framework}".camelize.constantize
@@ -51,8 +53,7 @@ module Apotomo
 
       # call existing widget
       # - widget_class_call :top_bar, :update, item: 1
- 
-      # --> Widget.TopBar.update('action': 1)
+      # --> Widget.TopBar.update('item': 1)
       def widget_class_call id, function, hash
         function_name = jq_helper.js_camelize function
         widget_name = jq_helper.ns_name id.to_s.camelize
@@ -61,15 +62,18 @@ module Apotomo
       end
  
       # call existing widget
-      # - widget_call :top_bar, :flash_light, action: 'search'
- 
+      # - widget_call :top_bar, :flash_light, action: 'search' 
       # --> Widgets.topBar.flashLight('action': 'search')
- 
       def widget_call id, function, hash
         function_name = jq_helper.js_camelize function
         "Widgets.#{id}.#{function_name}(#{hash.to_json});"
       end
 
+      protected
+
+      def jq_helper
+        JqueryHelper
+      end
     end
   end
 end
